581f6b66375a6e1d40aaf321016629307cf7b975
StebivkaYrii <markuius54621490@gmail.com>
2022-04-16 17:16:27 +0300

    master

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@
 export const App = () => {
   return (
       <div>
-        <div className={'wrap'}>
-          <Users/>
-          <Posts/>
-        </div>
-          <div className={'coms'}>
-              <Comments/>
-          </div>
+
       </div>
   );
 }
