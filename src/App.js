ffba38e38ac72ec5ea8716cc25b4b6a0a7198576
StebivkaYrii <markuius54621490@gmail.com>
2022-05-04 19:31:07 +0300

    9

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-
-import { MainLayout } from './layouts';
-import { CarsPage, NotFoundPage, SingleCarPage } from './pages';
-
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<MainLayout/>}>
-                <Route index element={<Navigate to={"cars"}/>}/>
-                <Route path="cars/:carId" element={<SingleCarPage/>}/>
-                <Route path="cars" element={<CarsPage/>}/>
-                <Route path="*" element={<NotFoundPage/>}/>
-            </Route>
-        </Routes>
+        <div>
+            App
+        </div>
     );
 };
 
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export * from './CarsPage/CarsPage';
-export * from './NotFoundPage/NotFoundPage';
-export * from './SingleCarPage/SingleCarPage';
